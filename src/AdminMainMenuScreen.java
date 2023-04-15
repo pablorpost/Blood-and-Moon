d9ead7b77dca4be875e8c0429d073db02b1e4de1
daaaviid-03 <d.pimentel.2021@alumnos.urjc.es>
2023-04-15 16:56:15 +0200

    Planteamiento de clases moradas

--- src/AdminMainMenuScreen.java
+++ src/AdminMainMenuScreen.java
@@ -1,2 +1,28 @@
-public class AdminMainMenuScreen {
+import java.util.List;
+import java.util.Map;
+
+public class AdminMainMenuScreen extends Screen{
+
+    private Admin admin;
+    private Map<String, List<String>> options;
+
+    public int showOptions(String options){
+        return 0;
+    }
+
+    public void validatRequests(){
+
+    }
+
+    public void AdminMainMenuScreen(DBManager dataBase, Store store){
+
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public Map<String, List<String>> getOptions() {
+        return options;
+    }
 }
