53067d2eb9edce3728c5ad290404495b371c109a
daaaviid-03 <d.pimentel.2021@alumnos.urjc.es>
2023-04-18 13:28:25 +0200

    inRegMenu a medias

--- src/InRegMenuScreen.java
+++ src/InRegMenuScreen.java
@@ -1,34 +1,77 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class InRegMenuScreen extends Screen{
 
     private Map<String, List<String>> options;
     private Map<String, List<String>> formVersions;
-    public Map<String, List<String>> getOptions() {
-        return options;
-    }
-
 
     public InRegMenuScreen(DBManager dataBase, Store store) {
-        setStore(store);
-        setDataBase(dataBase);
+        super.setStore(store);
+        super.setDataBase(dataBase);
+        super.setTitle("");
+
     }
+
 
 
     public int showOptions(String option){
-        return 0; //Vacío
+        List<String> thisOptions = options.get(option);
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(Integer.toString(i) + ".    " + thisOptions.get(i));
+        }
+        String election;
+        Scanner teclado = new Scanner(System.in);
+        election = teclado.nextLine();
+        return Integer.valueOf(election);
     }
 
     public Map<String, String> showForm(boolean isRegist, boolean isAdmin){
+        Map<String, String> formulario = new HashMap<String, String>();
+        if (isAdmin){
+            System.out.println("Admin Login...");
+        } else {
+            System.out.println("User Login...");
+        }
+        System.out.println("User/Nick:");
+        String election;
+        Scanner teclado = new Scanner(System.in);
+        election = teclado.nextLine();
+        formulario.put("nick", election);
+
+        if (isRegist){
+            System.out.println("Name:");
+            teclado = new Scanner(System.in);
+            election = teclado.nextLine();
+            formulario.put("name", election);
+        }
+
+        System.out.println("Password:");
+        teclado = new Scanner(System.in);
+        election = teclado.nextLine();
+        formulario.put("pas", election);
+
+        if (isRegist){
+            this.addPerson(formulario.get("nick"), formulario.get("name"), formulario.get("pas"), isAdmin);
+        } else {
+            DataBaseResult resulta = this.checkPerson(formulario.get("nick"), formulario.get("pas"));
+            if (resulta == DataBaseResult.user || resulta == DataBaseResult.admin){
+                Person thisPerson = this.getPersonAndSet(formulario.get("nick"), formulario.get("pas"));
+
+                // por terminar------------------------------------
+
+            }
+        }
         return null;
     }
 
-    public DataBaseResult checkPerson(String name, String pass){
+    public DataBaseResult checkPerson(String nick, String pass){
         return null;
     }
 
-    public Person addPerson(String name, String pass, boolean isAdmin){
+    public Person addPerson(String nick, String name, String pass, boolean isAdmin){
         return null;
     }
 
@@ -37,15 +80,16 @@ public class InRegMenuScreen extends Screen{
     }
 
     public boolean checkAdmin(int pas){
-        return false; //Vacío
+        return false;
     }
 
     public Person getPersonAndSet(String name, String pass){
         return null;
     }
 
-
-
+    public Map<String, List<String>> getOptions() {
+        return options;
+    }
 
     public void setOptions(Map<String, List<String>> options) {
         this.options = options;
