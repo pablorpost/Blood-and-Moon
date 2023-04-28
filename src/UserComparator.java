9a342c4c3b2fc08a32523b36cc83b0ce2a917011
magarciam2003 <ma.garciam.2021@alumnos.urjc.es>
2023-04-28 19:48:03 +0200

    comentarios

--- src/UserComparator.java
+++ src/UserComparator.java
@@ -1,6 +1,8 @@
 import java.util.Comparator;
 
 public class UserComparator implements Comparator {
+
+    //Establecerá que los usuarios se compararán en base al oro del que dispongan.
     @Override
     public int compare(Object o1, Object o2) {
         User u1 = (User) o1;
