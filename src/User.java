fb456e9ed1f56172c15a450f80ff82dc1d6da713
Diego-G19 <di.fernandez.2021@alumnos.urjc.es>
2023-04-28 21:18:01 +0200

    comentarios

--- src/User.java
+++ src/User.java
@@ -12,6 +12,7 @@ public class User extends Person{
     private List<Battle> battles;
     private int gold;
 
+    //Constructor
     public User(String nick, String name, int password){
         super(nick,name,password,DataBaseResult.user);
         this.weapons = new ArrayList<>();
@@ -20,76 +21,76 @@ public class User extends Person{
         character = null;
         gold = 500;
     }
-    public void uploadBattles(Battle value){
-        this.battles.add(value);
-    }
 
+    //Devolverá el personaje del user
     public Character getCharacter() {
         return character;
     }
 
+    //Modificará el personaje del user
     public void setCharacter(Character character) {
         this.character = character;
     }
 
+    //Devolverá la lista de armas activas
     public List<String> getWeapons() {
         return weapons;
     }
 
-    public void setWeapons(List<String> weapons) {
-        this.weapons = weapons;
-    }
-
+    //Devolverá el armadura activa
     public String getArmor() {
         return armor;
     }
 
+    //Asignará el armadura activa
     public void setArmor(String armor) {
         this.armor = armor;
     }
 
+    //Dirá si el user está o no baneado
     public boolean isBan() {
         return ban;
     }
 
+    //Asignará el ban del usuario
     public void setBan(boolean ban) {
         this.ban = ban;
     }
 
+    //Devolverá la lista de solicitudes pendientes
     public List<String> getPendingRequest() {
         return pendingRequest;
     }
 
+    //Asignará la lista de solicitudes pendientes
     public void setPendingRequest(List<String> pendingRequest) {
         this.pendingRequest = pendingRequest;
     }
 
+    //Devolverá la lista de las batallas
     @Override
     public List<Battle> getBattles() {
         return battles;
     }
 
-    @Override
-    public void setBattles(List<Battle> battles) {
-        this.battles = battles;
-    }
+    //Añadirá una batalla a la lista
     public void addBattle(Battle battle) {
         this.battles.add(battle);
     }
 
+    //Añadirá un arma a la lista
     public void addWeapon(String weapon) {
         this.weapons.add(weapon);
     }
 
+    //Devolverá el oro
     public int getGold() {
         return gold;
     }
 
+    //Asignará el valor de oro
     public void setGold(int gold) {
         this.gold = gold;
     }
 
-    public int compare(User o2) {
-        return this.getGold() - o2.getGold();
-    }
 }
