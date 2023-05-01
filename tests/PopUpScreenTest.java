77aaed1d68690cd4bb474db2e57040415bb8d391
pablorpost <pablorepost@gmail.com>
2023-05-01 18:33:06 +0200

    tremenda mierda de test

--- tests/PopUpScreenTest.java
+++ tests/PopUpScreenTest.java
@@ -1,22 +1,38 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PopUpScreenTest {
-    ByteArrayInputStream
 
 
-
-    @BeforeEach
-    void setUp(){
-        PopUpScreen popUp = new PopUpScreen(null, null, new User("","",0));
-    }
 
     @Test
-    @DisplayName()
+    @DisplayName("")
+    void test1(){
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream("3\n4\n".getBytes()));
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(byteArrayOutputStream);
+        PrintStream stdout = System.out;
+        System.setOut(ps);
+
+
+        System.setIn(stdin);
+        System.setOut(stdout);
+        System.setIn(new ByteArrayInputStream("0".getBytes()));
+        Manager man = new Manager(new Store());
+        PopUpScreen popUp = new PopUpScreen(null, man, new User("","",0));
+        ScreenResult r = popUp.showPopUp(0);
+        assertEquals(ScreenResult.stay,r);
+    }
 
 }
