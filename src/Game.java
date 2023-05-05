dc171150aa715ca27f8e48b0b1468d5e5983563f
pablorpost <pablorepost@gmail.com>
2023-05-05 17:30:22 +0200

    verificable random ( RandomGenerator.java )

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 public class Game {
     //crea la tienda y manager que respectivamente cargaran los datos de, y gestionaran el juego
     public void run(){
+        RandomGenerator.initRandomGenerator();
+        RandomGenerator.setSeed(1234);
         Store store = new Store();
         Manager manager = new Manager(store);
     }
---
+++ src/RandomGenerator.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+import java.util.Random;
+public class RandomGenerator {
+    public static Random RANDOM_OBJ;
+
+    public static void initRandomGenerator(){
+        RANDOM_OBJ = new Random(System.currentTimeMillis());
+    }
+
+    public static void setSeed(long seed){
+        RANDOM_OBJ.setSeed(seed);
+        System.out.println(seed);
+    }
+}
